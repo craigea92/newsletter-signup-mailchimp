5499c67965998ec61e8f09dd0a354280758d053a
Craig Eaton <craigea92@gmail.com>
2023-02-03 18:56:32 +0000

    Updated the post to pull the information from the client, convert it to a JSON and then push it to MailChimp

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const { privateInfo } = require("./apiKey");
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -12,12 +14,38 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email)
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
 
+  const jsonData = JSON.stringify(data)
+  const url = "https://us18.api.mailchimp.com/3.0/lists/" + privateInfo.listId
+  const options = {
+    method: "POST",
+    auth: "craig:" + privateInfo.apiKey
+  }
+  
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
 
 })
 
